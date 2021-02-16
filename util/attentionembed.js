fb5077065cf9b283ca5ea36281166fbcd9ec8e92
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-02-16 14:00:27 +0530

    Add files via upload

---
+++ util/attentionembed.js
@@ -0,0 +1,24 @@
+const { Client, Collection, MessageEmbed } = require("discord.js");
+const approveemoji = process.env.aemoji;
+const denyemoji = process.env.demoji;
+const PREFIX = process.env.mainprefix;
+
+
+module.exports = {
+ async  attentionembed(message, titel) {
+
+    try{
+      await message.reactions.removeAll();
+       await message.react(process.env.demoji);
+      }catch{
+        }
+
+    let resultsEmbed = new MessageEmbed()
+      .setTitle(":x: " + titel)
+      .setColor(process.env.embcol)
+      
+      message.channel.send(resultsEmbed);
+    return;
+
+  }
+};
