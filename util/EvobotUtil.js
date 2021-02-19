7fe3cbe8bd713b74b1884d35640c487b8d5fce4b
DURGESH KUMAR <77738041+DURGESHKS@users.noreply.github.com>
2021-02-19 15:03:32 +0530

    Update EvobotUtil.js

--- util/EvobotUtil.js
+++ util/EvobotUtil.js
@@ -1,3 +1,5 @@
 exports.TOKEN = config ? config.TOKEN : process.env.bottoken;
 
 exports.PREFIX = config ? config.PREFIX : process.env.mainprefix;
+
+exports.VOLUME = config ? config.VOLUME : process.env.volume;
